6a879e01d9595830761f4de3e581118f0a1684f7
souza-luiza <souza.luiza@unifesp.br>
2026-05-20 05:36:32 +0000

    Mudancas nos banners

--- minix/kernel/main.c
+++ minix/kernel/main.c
@@ -342,6 +342,10 @@ static void announce(void)
 #endif
       "Copyright 2016, Vrije Universiteit, Amsterdam, The Netherlands\n",
       OS_RELEASE);
+  printf("========================================\n");
+  printf("| Minix 3.4.0rc6 - SO - UNIFESP 1s2026 |\n");
+  printf("|    Projeto Pratico (I) - Equipe 6    |\n");
+  printf("========================================\n");
   printf("MINIX is open source software, see http://www.minix3.org\n");
 }
 
@@ -358,6 +362,10 @@ void prepare_shutdown(const int how)
    * argument passes the shutdown status. 
    */
   printf("MINIX will now be shut down ...\n");
+  printf("=======================================\n");
+  printf("| ATE A PROXIMA - SO - UNIFESP 1s2026 |\n");
+  printf("|    Projeto Pratico (I) - Equipe 6   |\n");
+  printf("=======================================\n");
   set_kernel_timer(&shutdown_timer, get_monotonic() + system_hz,
       minix_shutdown, how);
 }
