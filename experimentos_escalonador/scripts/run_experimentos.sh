c9f60e06982eb37cb292eac9e4a1beeda546017f
Viviane Park <viviflorp@gmail.com>
2026-05-22 19:44:08 -0300

    script experimentos

---
+++ experimentos_escalonador/scripts/run_experimentos.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+# Runner script extracted from Makefile to improve POSIX compatibility on Minix
+# Usage: run_experimentos.sh "<scenarios>" <IO_OPS> <CPU_OPS> <REPETICOES> <EXECUTAVEL> <RESULTADOS>
+
+SCENARIOS="$1"
+shift
+IO_OPS="$1"; shift
+CPU_OPS="$1"; shift
+REPETICOES="$1"; shift
+EXECUTAVEL="$1"; shift
+RESULTADOS="$1"; shift
+
+for qtde in $SCENARIOS; do
+    dir="$RESULTADOS/cenario_$qtde"
+    mkdir -p "$dir"
+    printf 'cenario=%s\nio_ops=%s\ncpu_ops=%s\nrepeticoes=%s\n\nexecucao\tmedia_io\tmedia_cpu\n' "$qtde" "$IO_OPS" "$CPU_OPS" "$REPETICOES" > "$dir/resumo.txt"
+    total_io=0
+    total_cpu=0
+    cont_io=0
+    cont_cpu=0
+    repeticao=1
+    while [ "$repeticao" -le "$REPETICOES" ]; do
+        stdout="$dir/exec_$repeticao.stdout"
+        stderr="$dir/exec_$repeticao.stderr"
+        printf 'execucao=%s\n' "$repeticao" > "$stdout"
+        ./"$EXECUTAVEL" "$qtde" "$IO_OPS" "$CPU_OPS" >> "$stdout" 2> "$stderr"
+        media_io=$(awk '/^IO[[:space:]]/ { soma += $3; contador += 1 } END { if (contador > 0) printf "%.6f", soma / contador; else printf "0.000000" }' "$stdout")
+        media_cpu=$(awk '/^CPU[[:space:]]/ { soma += $3; contador += 1 } END { if (contador > 0) printf "%.6f", soma / contador; else printf "0.000000" }' "$stdout")
+        printf '%s\t%s\t%s\n' "$repeticao" "$media_io" "$media_cpu" >> "$dir/resumo.txt"
+        total_io=$(awk -v a="$total_io" -v b="$media_io" 'BEGIN { printf "%.6f", a + b }')
+        total_cpu=$(awk -v a="$total_cpu" -v b="$media_cpu" 'BEGIN { printf "%.6f", a + b }')
+        cont_io=$(awk -v a="$cont_io" 'BEGIN { printf "%d", a + 1 }')
+        cont_cpu=$(awk -v a="$cont_cpu" 'BEGIN { printf "%d", a + 1 }')
+        repeticao=$(expr "$repeticao" + 1)
+    done
+    media_final_io=$(awk -v a="$total_io" -v b="$cont_io" 'BEGIN { if (b > 0) printf "%.6f", a / b; else printf "0.000000" }')
+    media_final_cpu=$(awk -v a="$total_cpu" -v b="$cont_cpu" 'BEGIN { if (b > 0) printf "%.6f", a / b; else printf "0.000000" }')
+    printf '\nmedia_final\t%s\t%s\n' "$media_final_io" "$media_final_cpu" >> "$dir/resumo.txt"
+done
+
+exit 0
