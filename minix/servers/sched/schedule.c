59864140881ea2059d4a4c0a354c6c342e14721b
akemiikemoto <leticia.ikemoto@unifesp.br>
2026-05-24 14:28:44 -0300

    chore(sched): altera DEFAULT_USER_TIME_SLICE para 2 para teste de quantum curto

--- minix/servers/sched/schedule.c
+++ minix/servers/sched/schedule.c
@@ -38,7 +38,11 @@ static int schedule_process(struct schedproc * rmp, unsigned flags);
 
 #define cpu_is_available(c)	(cpu_proc[c] >= 0)
 
-#define DEFAULT_USER_TIME_SLICE 200
+//Quantum original do código 200
+//#define DEFAULT_USER_TIME_SLICE 200
+
+//Teste de modificação para um quantum muito pequeno 200->2
+#define DEFAULT_USER_TIME_SLICE 2
 
 /* processes created by RS are sysytem processes */
 #define is_system_proc(p)	((p)->parent == RS_PROC_NR)
