c37a253c05737bb2e166f1a788881d0c34ee31e4
akemiikemoto <leticia.ikemoto@unifesp.br>
2026-05-24 14:35:43 -0300

    chore(sched): altera DEFAULT_USER_TIME_SLICE para 1000 para teste de quantum longo
    - Altera a constante DEFAULT_USER_TIME_SLICE de 200 para 2 e 1000 no schedule.c.
    - Objetivo: Isolar o comportamento do Algoritmo round-robin com preempção diferente da original.
    - Preparação para a coleta de dados de desempenho com o teste_processos.

--- minix/servers/sched/schedule.c
+++ minix/servers/sched/schedule.c
@@ -42,7 +42,10 @@ static int schedule_process(struct schedproc * rmp, unsigned flags);
 //#define DEFAULT_USER_TIME_SLICE 200
 
 //Teste de modificação para um quantum muito pequeno 200->2
-#define DEFAULT_USER_TIME_SLICE 2
+//#define DEFAULT_USER_TIME_SLICE 2
+
+//Teste de modificação para um quantum muito grande 200->1000
+#define DEFAULT_USER_TIME_SLICE 1000
 
 /* processes created by RS are sysytem processes */
 #define is_system_proc(p)	((p)->parent == RS_PROC_NR)
