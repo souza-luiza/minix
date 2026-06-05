edd43c4a4b5be674d0cae1b1982820068a0a9b21
Enzo de Almeida Belfort Rizzi Di Chiara <belfortenzo@gmail.com>
2026-06-05 01:02:21 -0300

    removendo espacos do define

---
+++ minix/servers/schedule.c
@@ -0,0 +1,413 @@
+/* This file contains the scheduling policy for SCHED
+ *
+ * The entry points are:
+ * do_noquantum:        Called on behalf of process' that run out of quantum
+ * do_start_scheduling  Request to start scheduling a proc
+ * do_stop_scheduling   Request to stop scheduling a proc
+ * do_nice              Request to change the nice level on a proc
+ * init_scheduling      Called from main.c to set up/prepare scheduling
+ */
+#include "sched.h"
+#include "schedproc.h"
+#include <assert.h>
+#include <minix/com.h>
+#include <minix/syslib.h>
+#include <machine/archtypes.h>
+#include <minix/sysutil.h>
+
+static unsigned balance_timeout;
+
+#define BALANCE_TIMEOUT 5
+
+#define PROCESS_IN_USER_Q(x) ((x)->priority >= MAX_USER_Q && (x)->priority <= MIN_USER_Q) 
+
+static int schedule_process(struct schedproc * rmp, unsigned flags);
+static int do_lottery(void);
+static unsigned long gerar_numero_aleatorio(unsigned long max_tickets);
+
+static unsigned long prng_seed = 12345;
+
+#define SCHEDULE_CHANGE_PRIO    0x1
+#define SCHEDULE_CHANGE_QUANTUM 0x2
+#define SCHEDULE_CHANGE_CPU     0x4
+
+#define SCHEDULE_CHANGE_ALL (SCHEDULE_CHANGE_PRIO | SCHEDULE_CHANGE_QUANTUM | SCHEDULE_CHANGE_CPU)
+
+#define schedule_process_local(p) schedule_process(p, SCHEDULE_CHANGE_PRIO | SCHEDULE_CHANGE_QUANTUM)
+#define schedule_process_migrate(p) schedule_process(p, SCHEDULE_CHANGE_CPU)
+
+#define CPU_DEAD  -1
+
+#define cpu_is_available(c) (cpu_proc[c] >= 0)
+
+#define DEFAULT_USER_TIME_SLICE 200
+
+#define is_system_proc(p) ((p)->parent == RS_PROC_NR)
+
+static unsigned cpu_proc[CONFIG_MAX_CPUS];
+
+static void pick_cpu(struct schedproc * proc)
+{
+#ifdef CONFIG_SMP
+  unsigned cpu, c;
+  unsigned cpu_load = (unsigned) -1;
+  
+  if (machine.processors_count == 1) {
+    proc->cpu = machine.bsp_id;
+    return;
+  }
+
+  if (is_system_proc(proc)) {
+    proc->cpu = machine.bsp_id;
+    return;
+  }
+
+  cpu = machine.bsp_id;
+  for (c = 0; c < machine.processors_count; c++) {
+    if (!cpu_is_available(c))
+      continue;
+    if (c != machine.bsp_id && cpu_load > cpu_proc[c]) {
+      cpu_load = cpu_proc[c];
+      cpu = c;
+    }
+  }
+  proc->cpu = cpu;
+  cpu_proc[cpu]++;
+#else
+  proc->cpu = 0;
+#endif
+}
+
+/*===========================================================================*
+ * do_noquantum             *
+ *===========================================================================*/
+int do_noquantum(message *m_ptr)
+{
+  register struct schedproc *rmp;
+  int rv, proc_nr_n;
+
+  if (sched_isokendpt(m_ptr->m_source, &proc_nr_n) != OK) {
+    printf("SCHED: WARNING: got an invalid endpoint in OOQ msg %u.\n", m_ptr->m_source);
+    return EBADEPT;
+  }
+
+  rmp = &schedproc[proc_nr_n];
+
+  if (PROCESS_IN_USER_Q(rmp)) {
+    rmp->priority = USER_Q;
+  } else if (rmp->priority < MAX_USER_Q - 1) {
+    rmp->priority += 1;
+  }
+
+  if ((rv = schedule_process_local(rmp)) != OK) {
+    return rv;
+  }
+
+  if (PROCESS_IN_USER_Q(rmp)) {
+      if ((rv = do_lottery()) != OK) {
+        return rv;
+      }
+  }
+
+  return OK;
+}
+
+/*===========================================================================*
+ * do_stop_scheduling           *
+ *===========================================================================*/
+int do_stop_scheduling(message *m_ptr)
+{
+  register struct schedproc *rmp;
+  int proc_nr_n;
+
+  if (!accept_message(m_ptr))
+    return EPERM;
+
+  if (sched_isokendpt(m_ptr->m_lsys_sched_scheduling_stop.endpoint, &proc_nr_n) != OK) {
+    printf("SCHED: WARNING: got an invalid endpoint in OOQ msg %d\n", m_ptr->m_lsys_sched_scheduling_stop.endpoint);
+    return EBADEPT;
+  }
+
+  rmp = &schedproc[proc_nr_n];
+#ifdef CONFIG_SMP
+  cpu_proc[rmp->cpu]--;
+#endif
+  rmp->flags = 0;
+
+  return do_lottery();
+}
+
+/*===========================================================================*
+ * do_start_scheduling          *
+ *===========================================================================*/
+int do_start_scheduling(message *m_ptr)
+{
+  register struct schedproc *rmp;
+  int rv, proc_nr_n, parent_nr_n;
+  
+  assert(m_ptr->m_type == SCHEDULING_START || m_ptr->m_type == SCHEDULING_INHERIT);
+
+  if (!accept_message(m_ptr))
+    return EPERM;
+
+  if ((rv = sched_isemtyendpt(m_ptr->m_lsys_sched_scheduling_start.endpoint, &proc_nr_n)) != OK) {
+    return rv;
+  }
+  rmp = &schedproc[proc_nr_n];
+
+  rmp->endpoint     = m_ptr->m_lsys_sched_scheduling_start.endpoint;
+  rmp->parent       = m_ptr->m_lsys_sched_scheduling_start.parent;
+  rmp->max_priority = m_ptr->m_lsys_sched_scheduling_start.maxprio;
+  
+  if (rmp->max_priority >= NR_SCHED_QUEUES) {
+    return EINVAL;
+  }
+
+  if (rmp->endpoint == rmp->parent) {
+    rmp->priority   = USER_Q;
+    rmp->time_slice = DEFAULT_USER_TIME_SLICE;
+#ifdef CONFIG_SMP
+    rmp->cpu = machine.bsp_id;
+#endif
+  }
+  
+  switch (m_ptr->m_type) {
+  case SCHEDULING_START:
+    rmp->priority   = rmp->max_priority;
+    rmp->time_slice = m_ptr->m_lsys_sched_scheduling_start.quantum;
+    break;
+    
+  case SCHEDULING_INHERIT:
+    if ((rv = sched_isokendpt(m_ptr->m_lsys_sched_scheduling_start.parent, &parent_nr_n)) != OK)
+      return rv;
+
+    rmp->priority = schedproc[parent_nr_n].priority;
+    rmp->time_slice = schedproc[parent_nr_n].time_slice;
+    break;
+    
+  default: 
+    assert(0);
+  }
+
+  if (PROCESS_IN_USER_Q(rmp)) {
+      rmp->priority = USER_Q;
+      rmp->ticketsNum = 5; 
+  } else {
+      rmp->ticketsNum = 0;
+  }
+
+  if ((rv = sys_schedctl(0, rmp->endpoint, 0, 0, 0)) != OK) {
+    printf("Sched: Error taking over scheduling for %d, kernel said %d\n", rmp->endpoint, rv);
+    return rv;
+  }
+  rmp->flags = IN_USE;
+
+  pick_cpu(rmp);
+  while ((rv = schedule_process(rmp, SCHEDULE_CHANGE_ALL)) == EBADCPU) {
+    cpu_proc[rmp->cpu] = CPU_DEAD;
+    pick_cpu(rmp);
+  }
+
+  if (rv != OK) {
+    printf("Sched: Error while scheduling process, kernel replied %d\n", rv);
+    return rv;
+  }
+
+  m_ptr->m_sched_lsys_scheduling_start.scheduler = SCHED_PROC_NR;
+  return OK;
+}
+
+/*===========================================================================*
+ * do_nice              *
+ *===========================================================================*/
+int do_nice(message *m_ptr)
+{
+  struct schedproc *rmp;
+  int rv;
+  int proc_nr_n;
+  unsigned new_q, old_q, old_max_q;
+  int old_ticketsNum;
+  int absolute_tickets; 
+
+  if (!accept_message(m_ptr))
+    return EPERM;
+
+  if (sched_isokendpt(m_ptr->m_pm_sched_scheduling_set_nice.endpoint, &proc_nr_n) != OK) {
+    printf("SCHED: WARNING: got an invalid endpoint in nice msg %d\n", m_ptr->m_pm_sched_scheduling_set_nice.endpoint);
+    return EBADEPT;
+  }
+
+  rmp = &schedproc[proc_nr_n];
+  new_q = m_ptr->m_pm_sched_scheduling_set_nice.maxprio;
+  if (new_q >= NR_SCHED_QUEUES) {
+    return EINVAL;
+  }
+
+  old_q     = rmp->priority;
+  old_max_q = rmp->max_priority;
+  old_ticketsNum = rmp->ticketsNum;
+
+  rmp->max_priority = new_q;
+  
+  if (new_q >= MAX_USER_Q && new_q <= MIN_USER_Q) {
+      rmp->priority = USER_Q;
+      
+      absolute_tickets = (16 - new_q) * 5; 
+      if (absolute_tickets < 1) absolute_tickets = 1;
+      if (absolute_tickets > 100) absolute_tickets = 100;
+      
+      rmp->ticketsNum = absolute_tickets;
+  } else {
+      rmp->priority = new_q;
+      rmp->ticketsNum = 0;
+  }
+
+  if ((rv = schedule_process_local(rmp)) != OK) {
+    rmp->priority     = old_q;
+    rmp->max_priority = old_max_q;
+    rmp->ticketsNum   = old_ticketsNum;
+    return rv;
+  }
+
+  if (new_q >= MAX_USER_Q && new_q <= MIN_USER_Q) {
+      return do_lottery();
+  }
+
+  return OK;
+}
+
+/*===========================================================================*
+ * schedule_process           *
+ *===========================================================================*/
+static int schedule_process(struct schedproc * rmp, unsigned flags)
+{
+  int err;
+  int new_prio, new_quantum, new_cpu, niced;
+
+  pick_cpu(rmp);
+
+  if (flags & SCHEDULE_CHANGE_PRIO)
+    new_prio = rmp->priority;
+  else
+    new_prio = -1;
+
+  if (flags & SCHEDULE_CHANGE_QUANTUM)
+    new_quantum = rmp->time_slice;
+  else
+    new_quantum = -1;
+
+  if (flags & SCHEDULE_CHANGE_CPU)
+    new_cpu = rmp->cpu;
+  else
+    new_cpu = -1;
+
+  niced = (rmp->max_priority > USER_Q);
+
+  if ((err = sys_schedule(rmp->endpoint, new_prio, new_quantum, new_cpu, niced)) != OK) {
+    printf("PM: An error occurred when trying to schedule %d: %d\n", rmp->endpoint, err);
+  }
+
+  return err;
+}
+
+/*===========================================================================*
+ * init_scheduling            *
+ *===========================================================================*/
+void init_scheduling(void)
+{
+  int r, proc_nr;
+  struct schedproc *rmp;
+  clock_t tempo_atual; 
+
+  balance_timeout = BALANCE_TIMEOUT * sys_hz();
+
+  if ((r = sys_setalarm(balance_timeout, 0)) != OK)
+    panic("sys_setalarm failed: %d", r);
+
+  for (proc_nr = 0, rmp = schedproc; proc_nr < NR_PROCS; proc_nr++, rmp++) {
+    rmp->ticketsNum = 0;
+  }
+
+  if (getuptime(&tempo_atual) == OK) {
+      prng_seed = (unsigned long) tempo_atual;
+  } else {
+      prng_seed = 12345; 
+  }
+}
+
+/*===========================================================================*
+ * do_lottery            *
+ *===========================================================================*/
+static unsigned long gerar_numero_aleatorio(unsigned long max_tickets) {
+    prng_seed = (prng_seed * 279470273UL) % 4294967291UL;
+    return prng_seed % max_tickets;
+}
+
+static int do_lottery(void)
+{
+  struct schedproc *rmp;
+  int proc_nr;
+  int rv;
+  unsigned long lucky;
+  int old_priority;
+  int nTickets = 0;
+
+  for (proc_nr = 0, rmp = schedproc; proc_nr < NR_PROCS; proc_nr++, rmp++) {
+    if ((rmp->flags & IN_USE) && rmp->priority == MAX_USER_Q) {
+        rmp->priority = USER_Q;
+        schedule_process_local(rmp);
+    }
+  }
+
+  for (proc_nr = 0, rmp = schedproc; proc_nr < NR_PROCS; proc_nr++, rmp++) {
+    if ((rmp->flags & IN_USE) && PROCESS_IN_USER_Q(rmp) && USER_Q == rmp->priority) {
+      nTickets += rmp->ticketsNum;
+    }
+  }
+
+  if (nTickets == 0) return OK; 
+
+  lucky = gerar_numero_aleatorio((unsigned long)nTickets);
+
+  for (proc_nr = 0, rmp = schedproc; proc_nr < NR_PROCS; proc_nr++, rmp++) {
+    if ((rmp->flags & IN_USE) && PROCESS_IN_USER_Q(rmp) && USER_Q == rmp->priority) {
+      if (lucky < (unsigned long)rmp->ticketsNum) {
+          old_priority = rmp->priority;
+          rmp->priority = MAX_USER_Q;
+          
+          if (old_priority != rmp->priority) {
+            if ((rv = schedule_process_local(rmp)) != OK) {
+              return rv;
+            }
+          }
+          return OK; 
+      }
+      lucky -= rmp->ticketsNum;
+    }
+  }
+
+  return OK;
+}
+
+/*===========================================================================*
+ * balance_queues             *
+ *===========================================================================*/
+void balance_queues(void)
+{
+  struct schedproc *rmp;
+  int proc_nr, r;
+
+  for (proc_nr = 0, rmp = schedproc; proc_nr < NR_PROCS; proc_nr++, rmp++) {
+    if (rmp->flags & IN_USE) {
+      if (!PROCESS_IN_USER_Q(rmp)) {
+        if (rmp->priority > rmp->max_priority) {
+          rmp->priority -= 1;
+          schedule_process_local(rmp);
+        }
+      }
+    }
+  }
+
+  if ((r = sys_setalarm(balance_timeout, 0)) != OK)
+    panic("sys_setalarm failed: %d", r);
+}
